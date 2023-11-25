b61e8ea38b460bc2ddf86dd04b2427febd273cd9
RyanSolanki <ryansolanki1@gmail.com>
2023-11-25 13:19:52 -0800

    Added ui conmponenets to shipgrid ui

--- Port_Project/ship_grid.py
+++ Port_Project/ship_grid.py
@@ -41,6 +41,38 @@ class Ui_Form(QWidget):
         self.tableWidget.verticalHeader().setDefaultSectionSize(40)
         self.tableWidget.verticalHeader().setMinimumSectionSize(40)
         self.tableWidget.verticalHeader().setStretchLastSection(False)
+
+        #Adding label for Ship Grid
+        self.label_shipInventory = QtWidgets.QLabel(self)
+        self.label_shipInventory.setGeometry(QtCore.QRect(0, 40, 801, 41))
+        font = QtGui.QFont()
+        font.setPointSize(22)
+        font.setBold(True)
+        self.label_shipInventory.setFont(font)
+        self.label_shipInventory.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_shipInventory.setObjectName("label_shipInventory")
+        self.label_shipInventory.setText("Ship Inventory")
+
+        #Adding label for Ship Grid
+        self.label_shipGrid = QtWidgets.QLabel(self)
+        self.label_shipGrid.setGeometry(QtCore.QRect(0, 80, 801, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        self.label_shipGrid.setFont(font)
+        self.label_shipGrid.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_shipGrid.setObjectName("label_shipGrid")
+        self.label_shipGrid.setText("Please Select Any Blue Container To Remove From Ship")
+
+        # Adding the done button for when user is finished selecting containers to be removed
+        self.pushButton_removeDone = QtWidgets.QPushButton(self)
+        self.pushButton_removeDone.setGeometry(QtCore.QRect(330, 485, 141, 61))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.pushButton_removeDone.setFont(font)
+        self.pushButton_removeDone.setObjectName("pushButton_removeDone")
+        self.pushButton_removeDone.setText("Done")
+
         # List that holds containers selected to be removed from ship
         self.containers_remove = []
         # Checks if selection was changed on ship grid
@@ -58,9 +90,16 @@ class Ui_Form(QWidget):
         self.menuBar.addMenu(self.fileMenu)
         self.menuBar.addAction(self.fileMenu.menuAction())
         self.layout.setMenuBar(self.menuBar)
-
+        
+        #Connecting buttons to functions
+        self.pushButton_removeDone.clicked.connect(self.remove_done)
         self.fileMenu.aboutToShow.connect(self.show_main_window)
         
+    def remove_done(self):
+        #show window to input crates to load onto ship
+        #hide current window
+        pass
+
     def show_main_window(self):
         self.main_window = Ui_MainWindow()
         self.main_window.show()
