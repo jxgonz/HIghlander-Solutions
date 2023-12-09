52c6bd8ee64521947109f30fc0e83fba0964a459
Johan Gonzalez <johangonzalez7373@gmail.com>
2023-12-09 15:36:57 -0800

    fixed heursitic

--- Port_Project/ai.py
+++ Port_Project/ai.py
@@ -3,6 +3,7 @@ import numpy as np
 from container import Container
 import copy
 
+#node class
 class Node:
   def __init__(self, state, parent=None, g=0, h=0):
     self.state = state
@@ -13,16 +14,19 @@ class Node:
   def f(self):
     return self.g + self.h
 
-class Tree:
+#tree class
+class Tree: 
   def __init__(self, root):
     self.root = root
 
+#crane class
 class Crane:
   def __init__(self):
-    self.coordinates = (0,8)
-    self.empty = True
-    self.inBuffer = False
+    self.coordinates = (0,8)   #crane's current coordinataes
+    self.empty = True          #checks whether crane is holding container
+    self.inBuffer = False      #checks whether crane is in buffer
 
+#problem space
 class Problem:
   def __init__(self, ship, buffer, crane, offloadShip, loadShip):
     self.ship = ship
@@ -30,13 +34,6 @@ class Problem:
     self.crane = crane
     self.offloadShip = offloadShip
     self.loadShip = loadShip
-    self.parent = None
-    self.g = 0
-    self.h = 0
-    self.cost = 0
-  
-  def f(self):
-    return self.g + self.h
     
   def goal_test(self, state):
     if len(state.loadShip) == 0 and len(state.offloadShip) == 0 and state.ship[state.crane.coordinates[0]][state.crane.coordinates[1]].name == "UNUSED":
@@ -66,20 +63,36 @@ class Problem:
       result = state.crane_to_ship(copy_node)
   
     return result
-    
+  
+  #searches for closest container that needs to be offloaded
   def container_search(self, state, offload):
+    #checks every column in the grid
     for i in range(12):
+      #checks every container in the column
       for container in state[i]:
         if container.name in offload:
           return container.coordinates
     return None
   
-  def heuristic(self, node):
+  def heuristic(self, node, operator):
+    #mahattan distance: |x1 - x2| + |y1 - y2|
     start = node.parent.state.crane.coordinates
     end = node.state.crane.coordinates
-    return abs(start[0] - end[0]) + abs(start[1] - end[1])
+  
+    transfer = 0
+    # if node.parent.parent == None and end == (0, 8) and node.state.crane.empty == True:
+    #   transfer = 0
+    #if crane moves from and to truck, then add two
+    if operator == 1 or operator == 3:
+      transfer = 2
+      #if crane moves from and to buffer, then add four
+    elif operator == 2 or operator == 5:
+      transfer = 4
+    
+    return(abs(start[0] - end[0]) + abs(start[1] - end[1]) + transfer)
 
         
+  #if crane moves in between ship, then move container to closest unused cell
   def ship_to_ship(self, state):
     if state.crane.empty == False and state.crane.inBuffer == False and state.ship[state.crane.coordinates[0]][state.crane.coordinates[1]].name not in state.offloadShip:
       newCrane = state.crane
@@ -89,7 +102,9 @@ class Problem:
       newBuffer = state.buffer
       lhs = newCrane.coordinates[0] - 1
       rhs = newCrane.coordinates[0] + 1
+      #checks whether lhs and rhs is in between the bounds of the ship
       while lhs >= 0 or rhs <= 11:
+        #checks each cell in column lhs and rhs until the 9th level
         for i in range(9):
           if lhs >= 0:
             if newShip[lhs][i].name == "UNUSED":
@@ -97,6 +112,7 @@ class Problem:
               newShip[newCrane.coordinates[0]][newCrane.coordinates[1]].name = "UNUSED"
               newCrane.coordinates = newShip[lhs][i].coordinates
               newCrane.empty = True
+              newCrane.inBuffer = False
               return Problem(newShip, newBuffer, newCrane, newOffload, newLoad)
           if rhs <= 11:
             if newShip[rhs][i].name == "UNUSED":
@@ -104,6 +120,7 @@ class Problem:
               newShip[newCrane.coordinates[0]][newCrane.coordinates[1]].name = "UNUSED"
               newCrane.coordinates = newShip[rhs][i].coordinates
               newCrane.empty = True
+              newCrane.inBuffer = False
               return Problem(newShip, newBuffer, newCrane, newOffload, newLoad)
         lhs -= 1
         rhs += 1
@@ -121,6 +138,7 @@ class Problem:
         if len(newLoad) != 0:
           newCrane.coordinates = (0,8)
           newCrane.empty = False
+          newCrane.inBuffer = False
           newShip[0][8].name = newLoad[0]
           return Problem(newShip, newBuffer, newCrane, newOffload, newLoad)
         else:
@@ -131,6 +149,7 @@ class Problem:
           newShip[newCrane.coordinates[0]][newCrane.coordinates[1]].name = "UNUSED"
           newCrane.coordinates = (0,8)
           newCrane.empty = True
+          newCrane.inBuffer = False
           return Problem(newShip, newBuffer, newCrane, newOffload, newLoad)
         else:
           return None
@@ -200,13 +219,16 @@ class Problem:
       else:
         for i in range(12):
           for container in newShip[i]:
-            if container.name == "UNUSED" and newShip[0][8].name in newLoad:
-              container.name = newShip[0][8].name
-              newLoad.remove(newShip[0][8].name)
-              newShip[0][8].name = "UNUSED"
-              newCrane.coordinates = container.coordinates
-              newCrane.empty = True
-              return Problem(newShip, newBuffer, newCrane, newOffload, newLoad)
+            coordinates = state.container_search(newShip, newOffload)
+            if coordinates != None and i != coordinates[0]:
+              if container.name == "UNUSED" and newShip[0][8].name in newLoad :
+                container.name = newShip[0][8].name
+                newLoad.remove(newShip[0][8].name)
+                newShip[0][8].name = "UNUSED"
+                newCrane.coordinates = container.coordinates
+                newCrane.empty = True
+                newCrane.inBuffer = False
+                return Problem(newShip, newBuffer, newCrane, newOffload, newLoad)
         return None
     else:
       return None
@@ -252,14 +274,14 @@ class Problem:
       newShip = state.ship
       newBuffer = state.buffer
       container_name = newBuffer[newCrane.coordinates[0]][newCrane.coordinates[1]]
-      if newCrane.empty:
+      if newCrane.empty and len(newOffload) != 0:
         coordinates = state.container_search(newShip, newOffload)
         for container in list(reversed(newShip[coordinates[0]])):
-          if container.name != "UNUSED":
-            newCrane.coordinates = container.coordinates
-            newCrane.empty = False
-            newCrane.inBuffer = False
-            return Problem(newShip, newBuffer, newCrane, newOffload, newLoad)
+            if container.name != "UNUSED":
+              newCrane.coordinates = container.coordinates
+              newCrane.empty = False
+              newCrane.inBuffer = False
+              return Problem(newShip, newBuffer, newCrane, newOffload, newLoad)
       else:
         for i in range(12):
           for container in newShip[i]:
@@ -295,131 +317,45 @@ class Problem:
 
 
 
-# def uniform_cost(problem):
-#   maxLength = 0
-#   root = problem
-#   tree = Tree(root)
-#   frontier = {root}
-#   explored = set()
-
-#   while len(frontier) != 0:
-#     current_node = min(frontier, key=lambda node: node.f())
-#     maxLength = max(len(frontier), maxLength)
-#     frontier.remove(current_node)
-
-#     if problem.goal_test(current_node.buffer):
-#       # print(np.matrix(current_node.state))
-#       path=[]
-#       while current_node:
-#         path.append(current_node.ship)
-#         current_node = current_node.parent
-#       path.reverse()
-#       # for state in path:
-#       #   print(np.matrix(state))
-#       #   print()
-#       return path, maxLength
-  
-#     explored.add(current_node)
-#     # print(explored)
-
-#     for i in range(7):
-#       # print(current_node.coordinates)
-#       # print()
-#       new_node = problem.apply_operator(i, current_node.ship, current_node.buffer)
-
-#       # print(new_state)
-
-#       if new_node is None:
-#         continue
-
-#       print(np.matrix(new_node.ship))
-#       print()
-#       print()
-
-#       new_node.parent = current_node
-#       new_node.g = current_node.g + 1
-#       # new_node.h = problem.heuristic(new_state)
-
-#       if new_node in explored or new_node in frontier:
-#         continue
-      
-#       # print(new_node.state)
-#       # print()
-#       # print(np.matrix(new_node.ship))
-#       # print()
-#       # print()
-
-#       frontier.add(new_node)
-#       # print(frontier)
-#   return None, maxLength
-
 def uniform_cost(problem):
-  frontier = []
-  visited = []
+  maxLength = 0
+  root = Node(problem, None, 1, 1)
+  tree = Tree(root)
+  frontier = {root}
+  explored = set()
 
-  current_node = problem
-  frontier.append(current_node)
+  while len(frontier) != 0:
+    current_node = min(frontier, key=lambda node: node.f())
+    maxLength = max(len(frontier), maxLength)
+    frontier.remove(current_node)
 
-  iter = 0
-  while True:
-    if not frontier:
-      print("Not possible")
-      return None
-    
-    frontier.sort(key = lambda x: x.g, reverse=True)
-    current_node = frontier.pop()
-
-    if problem.goal_test(current_node):
-      # print(np.matrix(current_node.state))
+    if problem.goal_test(current_node.state):
       path=[]
       while current_node:
-        path.append(current_node.ship)
+        path.append(current_node)
         current_node = current_node.parent
       path.reverse()
-      for state in path:
-        print(np.matrix(state))
+      for node in path:
+        print(np.matrix(node.state.ship))
         print()
-      return path
-    
-    visited.append(current_node)
-    # print()
-    # for node in visited:
-    #   print(node.state)
-    # print()
+      return path, maxLength
+  
+    explored.add(current_node)
 
     for i in range(7):
-      # print(current_node.crane.coordinates)
-      # print()
-      new = problem.apply_operator(i, current_node)
-      in_lists = False
+      new_state = problem.apply_operator(i, current_node.state)
 
-      # if new:
-      #   print(current_node.crane.coordinates, new.crane.coordinates)
-      # else:
-      #   print(None)
+      if new_state is None:
+        continue
 
-      if new:
-        new.parent = current_node
-        new.g = current_node.g + 1
-        new.cost = new.g
+      new_node = Node(new_state, current_node, 0, 0)
+      new_node.g = current_node.g + 1
 
-        in_lists = any(node == new for node in frontier) #check if already in frontier or visited
-        if not in_lists:
-          in_lists = any(node == new for node in visited)
+      if new_node in explored or new_node in frontier:
+        continue
 
-        if not in_lists:
-          frontier.append(new)
-        else:
-          for i,node in enumerate(frontier):
-            if (node == new) and (new.cost < node.cost):
-              frontier[i] = new
-
-      iter += 1
-        
-      # print()
-      # for node in frontier:
-      #   print(node.newCrane.coordinates)
-      # print()
+      frontier.add(new_node)
+  return None, maxLength
 
 def a_star(problem):
   maxLength = 0
@@ -431,52 +367,33 @@ def a_star(problem):
   while frontier:
     current_node = min(frontier, key=lambda node: node.f())
     frontier.remove(current_node)
-    # maxLength = max(len(frontier), maxLength)
 
     if problem.goal_test(current_node.state):
-        # print(np.matrix(current_node.ship))
         path=[]
         while current_node:
-          path.append(current_node.state)
+          path.append(current_node)
           current_node = current_node.parent
         path.reverse()
-        for state in path:
-          print(np.matrix(state.ship))
-          print(state.h)
+        for node in path:
+          print(np.matrix(node.state.ship))
           print()
         return path, maxLength
     
     explored.add(current_node)
-    # for node in explored:
-    #   print(node.state)
-    
-    # print()
     
     for i in range(7):
-      # print(current_node.state.crane.coordinates)
-      # print()
       new_state = problem.apply_operator(i, current_node.state)
       
       if new_state is None:
         continue
       
-      # print(np.matrix(new_node.ship))
-      # print()
       new_node = Node(new_state, current_node, 1, 1)
       new_node.g = current_node.g + 1
-      new_node.h = problem.heuristic(new_node)
+      new_node.h = problem.heuristic(new_node, i)
       
       if new_node in explored:
         continue
-      
-      # print(new_node.state)
-      # print()
-      # print(new_node.h)
-      # print()
-      # print(np.matrix(new_node.ship))
-      # print()
-      # print()
-    
+
       if new_node in frontier:
         frontier_node = next(node for node in frontier if node.state == new_node.state)
         if new_node.f() < frontier_node.f():
@@ -484,11 +401,6 @@ def a_star(problem):
           frontier.add(new_node)
       else:
         frontier.add(new_node)
-      
-      # for node in frontier:
-      #   print(node.state)
-    
-      # print()
         
   return None, maxLength          
   
@@ -505,6 +417,7 @@ def driver():
   data[0] = pd.to_numeric(data[0])
   data[1] = pd.to_numeric(data[1])
 
+  # SHIP SETUP
   ship = []
   for r in range(12):
     row = []
@@ -513,7 +426,6 @@ def driver():
     ship.append(row)
     row = None
 
-  # SHIP SETUP
   k = 0
   for i in range(0,8):
     for j in range(0,12):
@@ -526,6 +438,7 @@ def driver():
   for i in range(0,12):
     ship[i][8] = Container("UNUSED", (i,8))
 
+  # BUFFER SETUP
   buffer = []
   for r in range(24):
     ro = []
@@ -533,17 +446,11 @@ def driver():
       ro.append(Container("UNUSED", (r,c)))
     buffer.append(ro)
     ro = None
-    
-  # print(np.matrix(ship))
-  # print()
-  
+
   crane = Crane()
-  problem = Problem(ship, buffer, crane, ["Cat"], ["Frogs"])
+  problem = Problem(ship, buffer, crane, ["Cat"], ["Frog"])
   
-  # print(uniform_cost(problem))
   # uniform_cost(problem)
   a_star(problem)
-  # print()
-  # print(np.matrix(ship))
 
 driver()
